7059cd5a82e581b773c0b3a32bb67585bd711edd
Brandy <bdettmer@gmail.com>
2015-01-10 23:35:54 -0800

    removed many console logs

--- tessel-webserver-demo/app.js
+++ tessel-webserver-demo/app.js
@@ -17,7 +17,7 @@ development: true              // enable development logging, useful for debuggi
     // refer to the IR module as m.ir, or the accelerometer module as m.accel
 
     modules.relay = require('./fake_relay.js'); // comment this out when deploying
-    console.log(modules.relay);
+    //console.log(modules.relay);
 
     tessel.led[0].output(1);
     tessel.led[1].output(1);
@@ -30,25 +30,25 @@ development: true              // enable development logging, useful for debuggi
 
 
 function get_climate_data(modules, res, send_climate){
-    console.log("in beginning of get climate data");
+    //console.log("in beginning of get climate data");
     var our_climate_var;
     climate = modules.climate;
     climate.readTemperature('f', function (err, temp) {             // f means use fahrenheit
         climate.readHumidity(function (err, humid) {
             //weather_string = 'Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH';
-            console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
+            //console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
             our_climate_var = {"temperature": temp, "humidity": humid};
             send_climate(our_climate_var, res);
-            console.log("just made send_climate callback");
+            //console.log("just made send_climate callback");
         });
     });
-    console.log("leaving get_climate_data");
+    //console.log("leaving get_climate_data");
 }
 
 
 function start_server(tessel, modules, router, fs){  // passing in tessel for dependency injection
 
-    console.log('app.js running');
+    //console.log('app.js running');
     var relay = modules.relay;
     //relay.toggle();
 
@@ -60,7 +60,7 @@ function start_server(tessel, modules, router, fs){  // passing in tessel for de
         .use('fs', fs)
         // Listen on port 8080
         .listen(8080);
-    console.log('tiny-router listening');
+    //console.log('tiny-router listening');
 
 
     // When the router gets an HTTP request at /leds/[NUMBER]
@@ -79,14 +79,14 @@ function start_server(tessel, modules, router, fs){  // passing in tessel for de
     });
 
     // visual indicator that server is up
-    console.log('Running Server');
+    //console.log('Running Server');
     tessel.led[0].output(0);
     tessel.led[1].output(0);
     tessel.led[2].output(0);
 }
 
 function toggle_lights(res, tessel, relay){
-    console.log('lights toggled');
+    //console.log('lights toggled');
     tessel.led[1].toggle();
     // Toggle relay channel 1
     relay.toggle(1, function toggleOneResult(err) {
@@ -99,11 +99,11 @@ function toggle_lights(res, tessel, relay){
 
 
 function toggle_water(res, tessel, relay){
-    console.log('water turned on');
+    //console.log('water turned on');
     // Toggle the LED (change to water)
     tessel.led[0].toggle();
     // Toggle relay channel 2
-    console.log(util.inspect(relay));
+    //console.log(util.inspect(relay));
     relay.toggle(2, function toggleOneResult(err) {
         if (err) console.log("Err toggling 2", err);
     });
@@ -112,9 +112,9 @@ function toggle_water(res, tessel, relay){
 }
 
 function led_button_click(req, res, modules, tessel){
-    console.log("button click");
+    //console.log("button click");
     // this code gets executed when there is a call back
-    console.log('which led?', req.body.led)
+    //console.log('which led?', req.body.led)
     // Grab the LED being toggled
     var index = req.body.led;
     // Toggle the LED
@@ -125,8 +125,8 @@ function led_button_click(req, res, modules, tessel){
 }
 
 function send_climate_data(climate_info, res){
-    console.log("in send climate data function");
-    console.log(util.inspect(climate_info));
+    //console.log("in send climate data function");
+    //console.log(util.inspect(climate_info));
 
     var string_to_send = JSON.stringify(climate_info);
 
